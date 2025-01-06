1bd6e97fd1584a1776cf210bc3b3236d0c111915
Dias-x <andre.d14x@gmail.com>
2025-01-06 12:43:22 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person person = new Person("Hans", Geschlecht.Männlich);
+
+        //Ausführung "Männlich"
+        Console.WriteLine($"Geschlecht: {person.Geschlecht}");
+        
+        //Ausführung des Wertes "0"
+        Console.WriteLine($"Geschlecht: {(int)person.Geschlecht}");
+    }
+}
+
+enum Geschlecht
+{ 
+    //Wert wird Automatisch zugewiesen 
+Männlich, // -> 0
+Weiblich, // -> 1
+Divers    // -> 2
+}
+
+
+class Person
+{
+    public string Name { get; set; }
+    public Geschlecht Geschlecht { get; set; }
+
+    public Person(string name, Geschlecht geschlecht)
+    {
+        Name = name;
+        Geschlecht = geschlecht;
+    }
+}
